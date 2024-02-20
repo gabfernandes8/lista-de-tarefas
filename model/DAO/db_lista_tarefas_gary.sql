79401a9dac3633075b5fd4b2c2ebdd9cc4b97d95
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2024-02-20 10:08:30 -0300

    favicon

--- model/DAO/db_lista_tarefas_gary.sql
+++ model/DAO/db_lista_tarefas_gary.sql
@@ -7,5 +7,16 @@ create table tbl_tarefas
 	id integer primary key auto_increment,
     titulo varchar(50),
     descricao text,
-    concluido boolean
+    concluido boolean,
+    usuario_id integer,
+    
+    foreign key (usuario_id) references tbl_usuarios(id)
+);
+
+create table tbl_usuarios
+(
+	id integer primary key auto_increment,
+    nome varchar(100),
+    email varchar(50),
+    senha varchar(10)
 );
