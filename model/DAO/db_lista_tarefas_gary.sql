fc2fc5927cfad6426d1e1dbf231b6123272dfc20
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2024-02-22 08:48:21 -0300

    dba

--- model/DAO/db_lista_tarefas_gary.sql
+++ model/DAO/db_lista_tarefas_gary.sql
@@ -7,5 +7,63 @@ create table tbl_tarefas
 	id integer primary key auto_increment,
     titulo varchar(50),
     descricao text,
-    concluido boolean
+    concluido boolean,
+    usuario_id integer,
+    
+    foreign key (usuario_id) references tbl_usuarios(id)
 );
+
+create table tbl_usuarios
+(
+	id integer primary key auto_increment,
+    nome varchar(100),
+    email varchar(50),
+    senha varchar(10)
+);
+
+insert into tbl_usuarios (nome, email, senha)values
+("Ryan Alves", "ryan@email.com", "123");
+
+-- Pesquisas --
+
+select * from tbl_tarefas where usuario_id = 1;
+
+select * from tbl_tarefas where usuario_id = 1 and concluido = false;
+
+select * from tbl_tarefas where id = 1 and usuario_id = 1;
+
+update tbl_tarefas set concluido = true where id = 1 and usuario_id = 1;
+
+update tbl_tarefas set concluido = false where id = 1 and usuario_id = 1;
+
+update tbl_tarefas set 
+					titulo = "Fazer compras",	
+                    descricao = "Ir no mercado e comprar chocolate"
+					where id = 1 and usuario_id = 1;
+        
+delete from tbl_tarefas where id = 2 and usuario_id = 1;
+
+insert into tbl_tarefas (
+							titulo,
+                            descricao,
+                            concluido,
+                            usuario_id
+						) values (
+							"Fazer compras",
+							"Ir no mercado e comprar sorvete",
+							false,
+							1
+						);
+                        
+insert into tbl_tarefas (
+                                        titulo,
+                                        descricao,
+                                        concluido,
+                                        usuario_id
+                                        ) values (
+                                            'Fazer compras',
+                                            'Comprar chocolate',
+                                            false,
+                                            1
+                                        );
+                        
